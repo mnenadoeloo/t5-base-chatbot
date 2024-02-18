15e6b63ff504e9e035a1349a0fef8609be6c7143
Danat Karasaev <104631630+mnenadoeloo@users.noreply.github.com>
2024-02-18 22:13:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,10 +6,11 @@ from peft import PeftModel, PeftConfig
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 bot = telebot.TeleBot(token)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, 'Привет! Данный бот может вести диалог с тобой практически на все темы. К сожалению, пока что он неидеален, и поэтому может вести диалог только на английском языке.')
+    bot.send_message(message.chat.id, 'Привет! Данный бот может ответить тебе практически на все вопросы. К сожалению, пока что он неидеален, и поэтому может вести диалог только на английском языке.')
 
 
 @bot.message_handler(func=lambda message: True)
@@ -20,16 +21,16 @@ def handle_text(message):
     model = AutoModelForSeq2SeqLM.from_pretrained(config.base_model_name_or_path)
     tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 
-    model = PeftModel.from_pretrained(model, peft_model_name, device_map={"":0}).cuda()
+    model = PeftModel.from_pretrained(model, peft_model_name, device_map={"":0}).to(device)
     model.eval()
 
     sample = message.text
 
-    input_ids = tokenizer(sample, return_tensors="pt", truncation=True, max_length=256).input_ids.cuda()
+    input_ids = tokenizer(sample, return_tensors="pt", truncation=True, max_length=256).input_ids.to(device)
     outputs = model.generate(input_ids=input_ids, do_sample=True, top_p=0.9, max_length=256)
 
     response = tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0]
 
-    bot.reply_to(message, f'Response: {response}')
+    bot.reply_to(message, response)
 
 bot.infinity_polling()
